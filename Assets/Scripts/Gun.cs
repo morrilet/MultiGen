6191ac9ccfdf30ed14b3adb507cc50c61e86daa5
JIH7 <JIHlucky7@gmail.com>
2016-04-15 23:50:27 -0500

    Added skeletal gun script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public bool isAuto;
+
+	public int bulletsPerShot;
+
+	public float fireRate;
+	float fireRateCounter;
+
+	public float rotationDeviation;
+
+	public float bulletSpeed;
+	public float bulletSpeedDeviation;
+
+	public GameObject bullet;
+	public GameObject muzzleFlash;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	public void Shoot ()
+	{
+		
+	}
+
+	void InstantiateBullet ()
+	{
+		
+	}
+}
