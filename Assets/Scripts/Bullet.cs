44950a574df87a86182fd30a9eea612f421f2d1b
JIH7 <JIHlucky7@gmail.com>
2016-04-16 13:24:19 -0500

    Added basic GUN functionality

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	[HideInInspector]
+	public float bulletSpeed; //Speed of bullet
+	[HideInInspector]
+	public float bulletSpeedDeviation; //Used for shotguns
+	[HideInInspector]
+	public float maxRange;
+	[HideInInspector]
+	public float damage;
+	[HideInInspector]
+	public float sleepFramesOnHit;
+
+//	public GameObject impactEffect;
+	[HideInInspector]
+	public Vector3 startPos;
+
+	void Start()
+	{
+		startPos = transform.position;
+		bulletSpeed += Random.Range (-1 * bulletSpeedDeviation, bulletSpeedDeviation);
+	}
+
+	void Update () 
+	{
+		transform.position += bulletSpeed * transform.right * Time.deltaTime;
+
+		if (maxRange <= Mathf.Abs(startPos.x - transform.position.x))
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Wall")
+		{
+//			GameObject impact = Instantiate (impactEffect, transform.position, transform.rotation) as GameObject;
+			Destroy (gameObject);
+		}
+
+		if (coll.gameObject.tag == "Enemy")
+		{
+			Camera.main.GetComponent<CameraFollowTrap> ().ScreenShake (.1f, .075f);
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -11,31 +11,40 @@ public class Gun : MonoBehaviour {
 	float fireRateCounter;
 
 	public float rotationDeviation;
-
-	public float bulletSpeed;
 	public float bulletSpeedDeviation;
 
-	public GameObject bullet;
+	public GameObject bulletPrefab;
 	public GameObject muzzleFlash;
 
-	void Start ()
+	//Bullet data
+	public float maxRange;
+	public float bulletSpeed;
+
+	void Start()
 	{
-		
+		fireRateCounter = fireRate;
 	}
 
 	void Update ()
 	{
+		fireRateCounter += Time.deltaTime;
+		HandleInput ();
+	}
+
+	void HandleInput ()
+	{
+		if (isAuto)
+		{
+			if(Input.GetButton("Fire") && fireRateCounter >= fireRate)
+				Shoot();
+		}
+		if (!isAuto)
+		{
+			if (Input.GetButtonDown ("Fire") && fireRateCounter >= fireRate)
+				Shoot ();
+		}
+
 		LookAtMouse ();
-	}
-
-	public void Shoot ()
-	{
-		
-	}
-
-	void InstantiateBullet ()
-	{
-		
 	}
 
 	void LookAtMouse()
@@ -49,5 +58,27 @@ public class Gun : MonoBehaviour {
 		rotation *= (180 / Mathf.PI);
 
 		transform.rotation = Quaternion.Euler (0, 0, rotation);
+	}
+
+	public void Shoot ()
+	{
+		fireRateCounter = 0;
+		for (int i = 0; i < bulletsPerShot; i++)
+		{
+			InstantiateBullet ();
+		}
+	}
+
+	void InstantiateBullet ()
+	{
+		Quaternion rotationHolder = new Quaternion ();
+		rotationHolder.eulerAngles = new Vector3 (0, 0, Random.Range (-rotationDeviation, rotationDeviation) + transform.rotation.eulerAngles.z);
+
+		GameObject bullet = Instantiate (bulletPrefab, new Vector3 (0, 0, 0), rotationHolder) as GameObject;
+
+		//Pass Parameters to instantiated bullet
+		bullet.GetComponent<Bullet> ().maxRange = maxRange;
+		bullet.GetComponent<Bullet> ().bulletSpeed = bulletSpeed;
+		bullet.GetComponent<Bullet> ().bulletSpeedDeviation = bulletSpeedDeviation;
 	}
 }
