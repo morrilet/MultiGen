af09170fd00fa10d7fe68134b021b214bfdeb303
JIH7 <JIHlucky7@gmail.com>
2016-04-15 23:24:35 -0500

    Added some basic scripts
    Added basic character controller, singleton pattern base class, and
    entity class. Also saved basic player prefab.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity
+{
+	public float speed;
+
+	Vector3 playerVelocity;
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+		HandleInput ();
+		Move (playerVelocity);
+	}
+
+	void HandleInput()
+	{
+		playerVelocity = new Vector3 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed, 0);
+	}
+
+	void Move(Vector3 velocity)
+	{
+		transform.position = Vector3.Lerp (transform.position, velocity + transform.position, Time.deltaTime);
+	}
+
+}
