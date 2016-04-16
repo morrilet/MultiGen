b9cc73b8036a4f27e050d7c468016587644f4783
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-04-16 15:35:09 -0700

    Added AIPathing Prefabs
    Added AIPathCell and AIPathDoor prefabs to the game. Next up is to
    generate them with the level.

---
+++ Assets/Scripts/AIPathCell.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIPathCell : MonoBehaviour 
+{
+	[HideInInspector]
+	public List<GameObject> doors = new List<GameObject>();
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "AIPathDoor") 
+		{
+			doors.Add (other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/AIPathDoor.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIPathDoor : MonoBehaviour 
+{
+	public GameObject[] cells;
+	public int[] doorsToCells;
+	public List<GameObject> immediateCells = new List<GameObject>();
+	bool testForCells = true;
+	float waitToTestCells = 2f;
+	int stage = 1;
+
+	public bool doorOpen = true;
+
+	void Awake()
+	{
+		doorOpen = true;
+		cells = GameObject.FindGameObjectsWithTag ("AIPathCell");
+		doorsToCells = new int[cells.Length];
+		testForCells = true;
+		waitToTestCells = 2;
+		stage = 1;
+	}
+
+	void Update()
+	{
+		if (testForCells && waitToTestCells <= 0) 
+		{
+			foreach (GameObject immediateCell in immediateCells) 
+			{
+				for (int i = 0; i <= cells.Length - 1; i++) 
+				{
+					if (cells [i] == immediateCell)
+						doorsToCells [i] = 1;
+				}
+			}
+
+			for (stage = 2; stage <= cells.Length; stage++) 
+			{
+				for (int i = 0; i <= cells.Length - 1; i++) 
+				{
+					if (doorsToCells [i] == stage - 1)
+						foreach (GameObject checkDoor in cells[i].GetComponent<AIPathCell>().doors) 
+						{
+							if (checkDoor != gameObject) 
+							{
+								foreach (GameObject checkCell in checkDoor.GetComponent<AIPathDoor>().immediateCells) 
+								{
+									for (int j = 0; j <= cells.Length - 1; j++) 
+									{
+										if (cells [j] == checkCell && doorsToCells [j] == 0)
+											doorsToCells [j] = stage;
+									}
+								}
+							}
+						}
+				}
+			}
+
+			testForCells = false;
+		}
+		waitToTestCells--;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "AIPathCell")
+			immediateCells.Add (other.gameObject);
+	}
+}
