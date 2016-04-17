cc910d3dbb6ff55301b9c3b38a551f63301804f6
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-04-16 18:17:54 -0700

    Added Audio Manager And Took Exit Time Off Of Player Animation
    Player animations now feel slightly more responsive. We also have an
    audio manager to play our sounds now. Woo!

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : Singleton<AudioManager> 
+{
+	public AudioClip[] effects;
+	public AudioClip[] music;
+
+	public AudioSource musicSource;
+	public AudioSource effectSource;
+
+	private float effectStartPitch; //The default pitch of the effect source.
+
+	public override void Awake()
+	{
+		isPersistant = true;
+
+		effectStartPitch = effectSource.pitch;
+
+		base.Awake ();
+	}
+
+	public void PlayMusic(string songName)
+	{
+		for (int i = 0; i < music.Length; i++) 
+		{
+			if (music [i].name == songName) 
+			{
+				musicSource.clip = music [i];
+				musicSource.loop = true;
+				musicSource.Play ();
+			}
+		}
+	}
+
+	public void PlaySoundEffect(string clipName)
+	{
+		for (int i = 0; i < effects.Length; i++) 
+		{
+			if (effects [i].name == clipName) 
+			{
+				effectSource.pitch = effectStartPitch;
+				effectSource.PlayOneShot (effects [i]);
+			}
+		}
+	}
+
+	public void PlaySoundEffectVariation(string clipName, float minPitch, float maxPitch)
+	{
+		float pitch = Random.Range (minPitch, maxPitch);
+		for (int i = 0; i < effects.Length; i++) 
+		{
+			if (effects [i].name == clipName) 
+			{
+				effectSource.pitch = pitch;
+				effectSource.PlayOneShot (effects [i]);
+			}
+		}
+	}
+}
