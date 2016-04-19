e966e0dd1d9af51ec337437bce66254267018593
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-04-18 21:24:53 -0700

    Made Footsteps Work For Lemmy
    Next up is to apply this fix to all characters.

--- Assets/Animations/StateScripts/BearWalkState.cs
+++ Assets/Animations/StateScripts/BearWalkState.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class BearWalkState : StateMachineBehaviour {
 
-	float soundTimer;
-	float soundTime;
+	bool footstep1Played;
+	bool footstep2Played;
+
+	float normalizedTime;
+	float prevNormalizedTime;
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
@@ -14,7 +17,48 @@ public class BearWalkState : StateMachineBehaviour {
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
 	{
-		AnimatorStateInfo state = animator.GetNextAnimatorStateInfo (0);
+		AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+		normalizedTime = state.normalizedTime;
+
+		/*
+		soundTime = state.length;
+
+		if (soundTimer > 0 && soundTimer < soundTime / 20f) 
+		{
+			AudioManager.instance.PlaySoundEffect("shot 1 (uncharged)");
+			Debug.Log("played");
+		}
+		if (soundTimer > soundTime * .5f && soundTimer < (soundTime * .5f) + (soundTime / 20f)) 
+		{
+			AudioManager.instance.PlaySoundEffect("shot 1 (uncharged)");
+			Debug.Log("played");
+		}
+
+		if (soundTimer > soundTime)
+			soundTimer = 0;
+
+		soundTimer += Time.deltaTime;
+		*/
+
+		if (Mathf.Round ((state.normalizedTime % 1) * 10) / 10f > .5f && !footstep2Played)
+		{
+			AudioManager.instance.PlaySoundEffectVariation("footstep 1", .97f, 1.3f);
+			footstep2Played = true;
+		}
+		if (Mathf.Round ((state.normalizedTime % 1) * 10) / 10f > 0f && !footstep1Played) 
+		{
+			AudioManager.instance.PlaySoundEffectVariation("footstep 1", .97f, 1.3f);
+			footstep1Played = true;
+		}
+
+		if (normalizedTime - (normalizedTime % 1) != prevNormalizedTime - (prevNormalizedTime % 1)) 
+		{
+			footstep1Played = false;
+			footstep2Played = false;
+		}
+
+		prevNormalizedTime = normalizedTime;
+		Debug.Log (Mathf.Round ((state.normalizedTime % 1) * 10) / 10f);
 	}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
