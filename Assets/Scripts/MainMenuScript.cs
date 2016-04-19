3b000a09aa7a0fcd9391e69edbd0ddb95970b1dd
jruffolo <jruffolo7@gmail.com>
2016-04-18 19:26:12 -0500

    MUSIC BUFF
    louder main menu music, added main music

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -6,18 +6,20 @@ public class MainMenuScript : MonoBehaviour
 {
 	public void Start ()
 	{
-		AudioManager.instance.PlayMusic ("menu music");
+		AudioManager.instance.PlayMusic ("menu music louder");
 	}
 
 	public void Play ()
 	{
 		AudioManager.instance.StopMusic ();
+		AudioManager.instance.PlayMusicWithIntro ("main music intro", "main music loop");
 		SceneManager.LoadScene ("LevelGenTest", LoadSceneMode.Single);
 	}
 
 	public void PlayTutorial()
 	{
 		AudioManager.instance.StopMusic ();
+		AudioManager.instance.PlayMusicWithIntro ("main music intro", "main music loop");
 		SceneManager.LoadScene ("Tutorial", LoadSceneMode.Single);
 	}
 
